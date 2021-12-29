fb2d4fa141fd6c175b66869d9f081f3c1a56c86e
Nguyễn Đức Tông <ductong@miennam24h.vn>
2021-12-29 14:59:03 +0700

    update Model database

---
+++ app/Models/Predict.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Predict extends Model
+{
+    protected $table = 'predict';
+    protected $fillable = [
+    ];
+    protected $primaryKey = 'id';
+}
