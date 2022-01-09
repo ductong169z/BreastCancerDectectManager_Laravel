7c2c9d36a436ae64f8f6e868a976432603e85224
dcongtoai <57785250+dcongtoai@users.noreply.github.com>
2022-01-09 15:37:30 +0700

    Update UsersController.php

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -96,7 +96,42 @@ class UsersController extends Controller
 
     public function passwordReset(User $user, UpdateUserRequest $request)
     {
-        
+        //  //Validate form
+        //  $validator = \Validator::make($request->all(),[
+        //     'oldpassword'=>[
+        //         'required', function($attribute, $value, $fail){
+        //             if( !\Hash::check($value, Auth::user()->password) ){
+        //                 return $fail(__('The current password is incorrect'));
+        //             }
+        //         },
+        //         'min:8',
+        //         'max:30'
+        //      ],
+        //      'newpassword'=>'required|min:8|max:30',
+        //      'cnewpassword'=>'required|same:newpassword'
+        //  ],[
+        //      'oldpassword.required'=>'Enter your current password',
+        //      'oldpassword.min'=>'Old password must have atleast 8 characters',
+        //      'oldpassword.max'=>'Old password must not be greater than 30 characters',
+        //      'newpassword.required'=>'Enter new password',
+        //      'newpassword.min'=>'New password must have atleast 8 characters',
+        //      'newpassword.max'=>'New password must not be greater than 30 characters',
+        //      'cnewpassword.required'=>'ReEnter your new password',
+        //      'cnewpassword.same'=>'New password and Confirm new password must match'
+        //  ]);
+
+        // if( !$validator->passes() ){
+        //     return response()->json(['status'=>0,'error'=>$validator->errors()->toArray()]);
+        // }else{
+             
+        //  $update = User::find(Auth::user()->id)->update(['password'=>\Hash::make($request->newpassword)]);
+
+        //  if( !$update ){
+        //      return response()->json(['status'=>0,'msg'=>'Something went wrong, Failed to update password in db']);
+        //  }else{
+        //      return response()->json(['status'=>1,'msg'=>'Your password has been changed successfully']);
+        //  }
+        // }
         return redirect()->route('users.index')
             ->withSuccess(__('User updated successfully.'));
     }
