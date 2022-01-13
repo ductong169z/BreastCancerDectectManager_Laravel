3d27b22c5882e479dd90591e3125dbd06153f4bc
Nguyễn Đức Tông <ductong@miennam24h.vn>
2022-01-13 10:58:27 +0700

    datatable

--- resources/views/predict/index.blade.php
+++ resources/views/predict/index.blade.php
@@ -2,7 +2,9 @@
 
 @section('main-content')
 
-
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.3/css/jquery.dataTables.css">
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css" />
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/buttons/1.5.2/css/buttons.dataTables.min.css" />
 <div class="bg-light p-4 rounded">
     <h2>Prediction</h2>
     <div class="lead">
@@ -32,20 +34,21 @@
             </div>
         </form>
     </div>
-    <table class="table table-striped">
+    <table class="table table-striped table-bordered" id="predict-table">
         <thead>
             <tr>
-                <th scope="col" width="15%">ID</th>
-
-                <th scope="col" width="15%">Doctor name</th>
-                <th scope="col" width="15%">Sonographer name</th>
-                <th scope="col" width="15%">Patients name</th>
-                <th scope="col" width="15%">Doctor confirmation</th>
-                <th scope="col" colspan="3" width="20%"></th>
+                <th>ID</th>
+                <th>Doctor name</th>
+                <th>Sonographer name</th>
+                <th>Patients name</th>
+                <th>Predict result</th>
+                <!-- <th></th> -->
             </tr>
         </thead>
         <tbody>
-            @foreach($predict as $item)
+        </tbody>
+        <!-- <tbody>
+@foreach($predict as $item)
                 <tr>
                     <td>{{ $loop->iteration }}</td>
                     <td>{{ $item->doctor_name }}</td>
@@ -62,12 +65,63 @@
                             class="btn btn-warning btn-sm">Delete</a>
                     </td>
                 </tr>
-            @endforeach
+@endforeach
            
-        </tbody>
+        </tbody> -->
     </table>
 
-    <!-- {{$predict->links()}} -->
+    <!-- {{ $predict->links() }} -->
 </div>
 
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"
+    integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.js"></script>
+
+<script>
+    $('#predict-table').DataTable({
+        searching: false,
+        "lengthMenu": [
+            [10, 25, 50, -1],
+            [10, 25, 50, "All"]
+        ],
+        dom: 'Bfrtip',
+        "processing": true,
+        "serverSide": true,
+        "responsive": true,
+        "deferRender": true,
+        "pagingType": "full_numbers",
+        "processing": true,
+        "serverSide": true,
+        "responsive": true,
+        "deferRender": true,
+        "ajax": {
+            "url": "{{ route('predict.api') }}",
+            "type": "get"
+        },
+        "columns": [{
+                data: 'id',
+                name: 'id'
+            },
+            {
+                data: 'doctor_name',
+                name: 'doctor_name'
+            },
+            {
+                data: 'sonographer_name',
+                name: 'sonographer_name'
+            },
+            {
+                data: 'patient_name',
+                name: 'patient_name'
+            },
+            {
+                data:'predict_result',
+                name:'predict_result'
+            }
+
+        ]
+
+    });
+
+</script>
 @endsection
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Predict;
+use Yajra\DataTables\DataTables;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,3 +25,15 @@ Route::post('image-to-base64', function (Request $request) {
     $base64 = base64_encode(file_get_contents($inputImage));
     return ['status' => true, 'predict' => "somthing else", 'image' => $base64];
 });
+ Route::get('predict/list',function(Request $request){
+    $predict = Predict::select('doctor.name as doctor_name', 'sonographer.name as sonographer_name', 'predict.predict_result', 'predict.id', 'patients.name as patient_name')
+    ->join('users as doctor', 'doctor.id', '=', 'doctor_id')
+    ->join('patients', 'patients.id', '=', 'patient_id')
+    ->join('users as sonographer', 'sonographer.id', '=', 'sonographer_id');
+    $patient=$request->patient;
+    if($patient){
+        $predict=$predict->where('patients.name', 'LIKE', '%' . $patient . '%');
+    }
+    $predict=$predict->get();
+    return Datatables::of($predict)->make(true);
+ })->name('predict.api');
