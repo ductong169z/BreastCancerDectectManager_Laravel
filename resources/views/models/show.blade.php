d0ce39cb75d87fb563896c8c53f24c22c0dd90f9
dcongtoai <57785250+dcongtoai@users.noreply.github.com>
2022-04-12 15:02:38 +0700

    update lại giao diện trang show
    các trang dc update:
     - User
     - Model
     - Patient

--- resources/views/models/show.blade.php
+++ resources/views/models/show.blade.php
@@ -2,27 +2,27 @@
 @extends('layouts.admin')
 
 @section('main-content')
-    <div class="bg-light p-4 rounded">
-        <h1>Show model</h1>
-        <div class="lead">
-
+<div class="bg-light p-4 rounded">
+    <h1 class="text-bold">Show model</h1>
+    <div class="container mt-4">
+        <div class="mb-3">
+            <label for="model name" class="form-label">Name</label>
+            <label class="form-control">{{ $model->name }}</label>
         </div>
-
-        <div class="container mt-4">
-            <div>
-                Name: {{ $model->name }}
-            </div>
-            <div>
-                File Name: {{ $model->file_name }}
-            </div>
-            <div>
-                Description: {{ $model->description }}
-            </div>
+        <div class="mb-3">
+            <label for="file name" class="form-label">File Name:</label>
+            <label class="form-control">{{ $model->file_name }}</label>
+            
         </div>
-
-    </div>
-    <div class="mt-4">
+        <div class="mb-3">
+            <label for="Description" class="form-label">Description</label>
+            <label class="form-control">{{ $model->description }}</label>
+        </div>
+        <div class="mt-4">
         <a href="{{ route('models.edit', $model->id) }}" class="btn btn-info">Edit</a>
         <a href="{{ route('models.index') }}" class="btn btn-default">Back</a>
     </div>
+    </div>
+</div>
+    
 @endsection
--- resources/views/patients/show.blade.php
+++ resources/views/patients/show.blade.php
@@ -1,35 +1,44 @@
 @extends('layouts.admin')
 
 @section('main-content')
-    <div class="bg-light p-4 rounded">
-        <h1>Show patient</h1>
-        <div class="lead">
+
+<div class="bg-light p-4 rounded">
+    <h1 class="text-bold">Show patient</h1>
+    <div class="container mt-4">
+        <div class="mb-3">
+            <label for="name" class="form-label">Name</label>
+            <label class="form-control">{{ $patient->name }}</label>
+        </div>
+        <div class="mb-3">
+            <label for="gender" class="form-label">Gender</label>
+            <label class="form-control">
+                @if($patient->gender =='1')
+                Male
+                @else
+                Female
+                @endif
+            </label>
 
         </div>
-
-        <div class="container mt-4">
-            <div>
-                Name: {{ $patient->name }}
-            </div>
-            @if($patient->gender =='1')
-                <div>Gender: Male</div>
-            @else
-                <div>Gender: Female</div>
-            @endif
-            <div>
-                ID Number: {{ $patient->id_number }}
-            </div>
-            <div>
-                Address: {{ $patient->address }}
-            </div>
-            <div>
-                Phone Number: {{ $patient->phone }}
-            </div>
+        <div class="mb-3">
+            <label for="id_number" class="form-label">ID number</label>
+            <label class="form-control">{{ $patient->id_number }}</label>
+        </div>
+        <div class="mb-3">
+            <label for="address" class="form-label">Address</label>
+            <label class="form-control">{{ $patient->address }}</label>
+        </div>
+        <div class="mb-3">
+            <label for="phone" class="form-label">Phone</label>
+            <label class="form-control">{{ $patient->phone }}</label>
         </div>
 
+        <div class="mt-4">
+            <a href="{{ route('patients.edit', $patient->id) }}" class="btn btn-info">Edit</a>
+            <a href="{{ route('patients.index') }}" class="btn btn-default">Back</a>
+        </div>
     </div>
-    <div class="mt-4">
-        <a href="{{ route('patients.edit', $patient->id) }}" class="btn btn-info">Edit</a>
-        <a href="{{ route('patients.index') }}" class="btn btn-default">Back</a>
-    </div>
+</div>
+
+
 @endsection
