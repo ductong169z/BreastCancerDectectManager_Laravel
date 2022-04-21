0f5dde2f2c5d2614d2217bf934e969c96625d985
Nguyễn Đức Tông <ductong@miennam24h.vn>
2022-04-21 21:31:21 +0700

    update

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -30,7 +30,14 @@
                     <div class="row">
                         <div class="col-lg-12">
                             <h5 class="font-weight-bold">About Breast Cancer Detection System</h5>
-                            <p class="text-justify">The Breast Cancer Detection System (BCDS) supports doctors diagnosing with an ultrasound breast image input. The system can diagnose and classify whether it is normal, or having a malignant or benign tumor. The system allows the doctors to create a request for patients to take an ultrasound scan to the sonographer. After finishing the scanning process, the sonographer uploads the ultrasound breast image into the system, then the machine learning model in BCDS diagnoses the result based on the given ultrasound image. Moreover, the BCDS identifies the cancer area on the inputted ultrasound image when returning the result to the doctor.</p>
+                            <p class="text-justify">The Breast Cancer Detection System (BCDS) supports doctors diagnosing
+                                with an ultrasound breast image input. The system can diagnose and classify whether it is
+                                normal, or having a malignant or benign tumor. The system allows the doctors to create a
+                                request for patients to take an ultrasound scan to the sonographer. After finishing the
+                                scanning process, the sonographer uploads the ultrasound breast image into the system, then
+                                the machine learning model in BCDS diagnoses the result based on the given ultrasound image.
+                                Moreover, the BCDS identifies the cancer area on the inputted ultrasound image when
+                                returning the result to the doctor.</p>
 
                         </div>
                     </div>
@@ -44,7 +51,7 @@
                             <p>Users, patients, predictions, and models management</p>
                             <p>Roles and permissions dynamic management</p>
                             <p>Data visualization</p>
-                        </div {{-- <div class="row">
+                        </div> {{-- <div class="row">
                         <div class="col-lg-12">
                             <h5 class="font-weight-bold">Credits</h5>
                             <p>Laravel SB Admin 2 uses some open-source third-party libraries/packages, many thanks to the web community.</p>
@@ -54,9 +61,72 @@
                                 <li><a href="https://startbootstrap.com/themes/sb-admin-2" target="_blank">SB Admin 2</a> - Thanks to Start Bootstrap.</li>
                             </ul>
                         </div>
-                    </div> --}} </div>
+                    </div> --}}
                     </div>
+                    <hr>
+                    <h2 style="text-align:center">Our Team</h2>
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="card">
+                                <img src="{{url('img/about/Luong_Hoang_Huong.png')}}" alt="Jane" style="width:100%">
+                                <div class="container">
+                                    <h5>Luong Hoang Huong</h5>
+                                    <p class="title">Supervisor</p>
+                                </div>
+                            </div>
+                        </div>
 
+                        <div class="col-md-4">
+                            <div class="card">
+                                <img src="{{url('img/about/Phan_Le_Trong_Nghia.png')}}" alt="Mike" style="width:100%">
+                                <div class="container">
+                                    <h5>Phan Le Trong Nghia</h5>
+                                    <p class="title">Leader</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-md-4">
+                            <div class="card">
+                                <img src="{{url('img/about/Dang_Minh_Thuan.png')}}" alt="John" style="width:100%">
+                                <div class="container">
+                                    <h5>Dang Minh Thuan</h5>
+                                    <p class="title">Member</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                     <div class="row">
+                        <div class="col-md-4">
+                            <div class="card">
+                                <img src="{{url('img/about/Nguyen_Duc_Tong.png')}}" alt="Jane" style="width:100%">
+                                <div class="container">
+                                    <h5>Nguyen Duc Tong</h5>
+                                    <p class="title">Member</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-md-4">
+                            <div class="card">
+                                <img src="{{url('img/about/Duong_Tri_Tin.png')}}" alt="Mike" style="width:100%">
+                                <div class="container">
+                                    <h5>Duong Tri Tin</h5>
+                                    <p class="title">Member</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-md-4">
+                            <div class="card">
+                                <img src="{{url('img/about/Dinh_Cong_Toai.png')}}" alt="John" style="width:100%">
+                                <div class="container">
+                                    <h5>Dinh Cong Toai</h5>
+                                    <p class="title">Member</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
 
             </div>
